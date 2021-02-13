07e809321f95652a7be735dd1e9d6b13fca0149e
Kadecahe <kadecahe@gmail.com>
2021-02-12 23:41:56 -0500

    FE: Added an image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import React from "react"
 export default function Home() {
   return (
     <div style={{color: 'purple'}}>
-     <hi>Hello Gatsby!</hi>
+     <h1>Hello Gatsby!</h1>
      <p>What a world.</p>
+     <img src="https://source.unsplash.com/random/400x200" alt="random-image" />
+
     </div>)
 }
